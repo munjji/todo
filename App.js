0ff96236c3f2201258a9a9d57bbe8e34bf28230b
Lee Jihee <129031670+munjji@users.noreply.github.com>
2024-08-15 16:01:45 +0900

    [FEAT] working 저장 기능 구현

--- App.js
+++ App.js
@@ -13,49 +13,68 @@ import { theme } from "./colors";
 import { useEffect, useState } from "react";
 
 const STORAGE_KEY = "@toDos";
+const HEADER = "header";
 
 export default function App() {
-  // work, travel인지 확인하기 위한 bool state
-  const [working, setWorking] = useState(true);
-  // TextInput으로 입력받은 text
+  const [working, setWorking] = useState(true); // 초기값은 false
   const [text, setText] = useState("");
-  // 입력받은 toDO들 저장
   const [toDos, setToDos] = useState({});
-  // work, travel 버튼 누르면 working 값 변경
-  const travel = () => setWorking(false);
-  const work = () => setWorking(true);
-  // text 변경되는 대로 저장
+
+  const travel = async () => {
+    setWorking(false);
+    await saveHeader(false);
+  };
+
+  const work = async () => {
+    setWorking(true);
+    await saveHeader(true);
+  };
+
   const onChangeEvent = (payload) => {
     setText(payload);
   };
+
   const saveToDos = async (toSave) => {
     await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(toSave));
   };
+
+  const saveHeader = async (working) => {
+    await AsyncStorage.setItem(HEADER, working.toString());
+  };
+
   const loadToDos = async () => {
     try {
       const jsonPayload = await AsyncStorage.getItem(STORAGE_KEY);
-      return jsonPayload != null ? setToDos(JSON.parse(jsonPayload)) : null;
+      if (jsonPayload) {
+        setToDos(JSON.parse(jsonPayload));
+      }
     } catch (error) {
       console.log(error);
     }
   };
 
+  const getHeader = async () => {
+    try {
+      const storageWorking = await AsyncStorage.getItem(HEADER);
+      setWorking(storageWorking === "true"); // 문자열 "true"와 비교
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   useEffect(() => {
-    loadToDos();
+    getHeader(); // AsyncStorage에서 header 값을 가져옴
+    loadToDos(); // ToDo 목록을 불러옴
   }, []);
 
-  // toDO 저장 및 추가 및 공백 처리
   const addToDo = async () => {
-    if (text === "") {
-      return;
-    }
+    if (text === "") return;
     const newToDos = { ...toDos, [Date.now()]: { text, working } };
     setToDos(newToDos);
     await saveToDos(newToDos);
     setText("");
   };
 
-  // toDo 삭제
   const deleteToDo = (key) => {
     Alert.alert("Delete To Do?", "Are you sure?", [
       { text: "Cancel" },
@@ -71,6 +90,7 @@ export default function App() {
       },
     ]);
   };
+
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
